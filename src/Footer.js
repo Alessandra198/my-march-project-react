b1a73254c0d6e588d68b16df364943a30a74323d
Alessandra198 <rinaldoalessandra@gmail.com>
2024-03-28 11:49:10 +0100

    Uploaded City.js and Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/Alessandra198/week-four-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      , by {""}
+      <a
+        href="https://github.com/Alessandra198"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Alessandra Rinaldo
+      </a>
+    </div>
+  );
+}
