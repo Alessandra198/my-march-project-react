ae8f2e90a78e1425eb69d3223ec52b7f9bb33213
Alessandra198 <rinaldoalessandra@gmail.com>
2024-03-28 11:51:36 +0100

    Uploaded the other file from my project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import Search from "./Search";
+import City from "./City";
+import Forecast from "./Forecast";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
+      <Search />
+      <City />
+      <Forecast />
+      <Footer />
     </div>
   );
 }
